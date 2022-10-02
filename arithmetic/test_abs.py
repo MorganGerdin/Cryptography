1445c41b75ed29d10e5351d56087abc09fe16723
Rumzei <12670264-Rumzei@users.noreply.gitlab.com>
2022-10-02 17:32:01 -0500

    Add comments clarifying whether native functions can be used to solve problems, + renames

--- arithmetic/test_abs.py
+++ arithmetic/test_abs.py
@@ -1,7 +1,8 @@
 # Returns the absolute value of x.
-def abs(x):
+# Note: you can't use Python's built-in absolute value function! :)
+def compute_absolute_value(x):
     return 0
 
 
 def test_abs():
-    assert abs(-5) == 5
+    assert compute_absolute_value(-5) == 5
--- arithmetic/test_min.py
+++ arithmetic/test_min.py
@@ -1,8 +1,9 @@
 # Returns the smallest number between x and y.
 # If both numbers are the same, x is returned.
-def min(x, y):
+# Note: you can't use Python's built-in min function :)
+def minimum(x, y):
     return 0
 
 
 def test_min():
-    assert min(5, 10) == 5
+    assert minimum(5, 10) == 5
