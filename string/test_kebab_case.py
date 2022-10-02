8f4dbcb1b574c8a707b88c9a7ef1c652dbee2267
Rumzei <12670264-Rumzei@users.noreply.gitlab.com>
2022-10-02 17:32:14 -0500

    Add test_kebab_case.py unit test file

--- string/test_kebab_case.py
+++ string/test_kebab_case.py
@@ -1,9 +1,9 @@
-# Converts and returns the input to PascalCase.
-# In PascalCase, the first character & characters after spaces are upper-cased.
-# All other characters are lower-cased. Spaces are removed.
-def to_pascal_case(input):
+# Converts and returns the input to kebab-case
+# kebab-case-looks-like-this!
+# Add dashes in place of spaces and before capital letters.
+def to_kebab_case(input):
     return input
 
 
 def test_pascal_case():
-    assert to_pascal_case("this is pascal case") == "ThisIsPascalCase"
+    assert to_kebab_case("This is KebabCase") == "this-is-kebab-case"
