861fbf87dfd8414767b253626a9750490dbf5332
“mjgerdin” <mjgerdin@students.unwsp.edu>
2022-10-05 10:53:36 -0500

    hello

--- arithmetic/test_sequential_integer.py
+++ arithmetic/test_sequential_integer.py
@@ -1,9 +1,17 @@
 # Return true if all individual numbers inside <i> are ordered from smallest to biggest.
 # Example: func(1223) is true, because 1 < 2 <= 2 < 3
 # Example: func(987) is false, because 9 is greater than 8
-def is_sequential_integer(i):
-    return False
 
+def is_sequential_integer(i):
+    i = str(i)
+    k = []
+    for item in i:
+        k.append(item)
+    new = sorted(k)
+    if k == new:
+        return True
+    else:
+        return False
 
 def test_is_sequential_integer():
     assert is_sequential_integer(123456789)
